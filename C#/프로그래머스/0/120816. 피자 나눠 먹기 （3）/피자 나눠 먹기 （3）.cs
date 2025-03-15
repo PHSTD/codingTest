1201cda49aa91e09c022b49762361496a80666e0
JGH <124040337+PHSTD@users.noreply.github.com>
2025-03-15 18:31:53 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.24 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int slice, int n) {
+            int answer = 0;
+            if (n % slice == 0) {
+                answer = n / slice;
+            } else {
+                answer = n / slice + 1;
+            }
+            return answer;
+        }
+}
