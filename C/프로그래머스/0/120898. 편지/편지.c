130f334d03b9b7260b78d0b68ebdf2793e73b063
JGH <124040337+PHSTD@users.noreply.github.com>
2025-03-17 22:56:55 +0900

    [level 0] Title: 편지, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ C/프로그래머스/0/120898. 편지/편지.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+int solution(const char* message) {
+    int answer = 0;
+    answer = strlen(message) * 2;
+    return answer;
+}
